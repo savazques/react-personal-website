a9c265e9c3a06d947477f38ba29b889b2e7199bf
Sarah Vasquez <savasquez@ucdavis.edu>
2024-06-23 16:12:49 -0700

    client credential token succes

--- serverB/serverB.js
+++ serverB/serverB.js
@@ -1,33 +1,36 @@
 const express = require('express');
 const SpotifyWebApi = require('spotify-web-api-node');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
+const clientId = '75b965ea1982472c8166015c2a4c48b9'; // Replace with your actual client ID
+const clientSecret = '755ea9059ae44d4bb2cdfe43d8b89df7'; // Replace with your actual client secret
 
-
-var  clientId = '75b965ea1982472c8166015c2a4c48b9',
-     clientSecret = '755ea9059ae44d4bb2cdfe43d8b89df7';
-
-var spotifyApi = new SpotifyWebApi({
-    cliendId: clientId,
-    clientSecret: clientSecret
+const spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
 });
 
-spotifyApi.clientCredentialsGrant().then (
-    function (data) {
-        console.log (data.body['expires_in'])
-        console.log('Access Token',data.body['access_token'])
+let accessToken = '';
 
-        spotifyApi.setAccessToken(data.body['access_token'])
+spotifyApi.clientCredentialsGrant().then(
+    function(data) {
+      console.log('Access Token:', data.body['access_token']);
+      console.log('Expires in:', data.body['expires_in']);
+  
+      spotifyApi.setAccessToken(data.body['access_token']);
     },
-    function (err) {
-        console.log("something went wrong", err);
+    function(err) {
+      console.log('Something went wrong', err);
     }
-)
+  );
+  
 
-const PORT = 3001;
+const PORT = 3002;
 app.listen(PORT, () => {
-  console.log(`Server A running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
