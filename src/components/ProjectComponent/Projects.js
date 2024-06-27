c4ce608f0fb34880927f4de89aaf6bf2b21c18e1
Sarah Vasquez <savasquez@ucdavis.edu>
2024-06-26 19:09:58 -0700

    added more project ui

--- src/components/ProjectComponent/Projects.js
+++ src/components/ProjectComponent/Projects.js
@@ -3,25 +3,44 @@ import "./Projects.css";
 
 function Projects () {
     return (
-        
-       <div className="main-container"> 
-
-        <h1> Featured Projects </h1>
-
-        <div className=" project-container">
-            <div className="project-links">
-                <a href="dsfds"><div className="project">Random Movie Generator</div></a>
-                <a href="sdf"><div className="project">Spotify Discover Saver </div></a>
-                <a href="sdf"><div className="project">Personal Website</div></a>
-            </div>
-
-
-        </div>
-
-
+       <div className="main-container">
+          <h1> Featured Projects </h1>
+          <div className="project-container">
+              <div className="project-links">
+                  <a href="dsfds" className="project-link">
+                      <div className="project">
+                          <div className="project-name">Travel App</div>
+                          <div className="project-description">
+                              Learn about different places!
+                              <br />
+                              Plan your next adventure with ease.
+                          </div>
+                      </div>
+                  </a>
+                  <a href="sdf" className="project-link">
+                      <div className="project">
+                          <div className="project-name">Music Discovery Saver</div>
+                          <div className="project-description">
+                              Discover new music!
+                              <br />
+                              Save your favorite tracks and playlists.
+                          </div>
+                      </div>
+                  </a>
+                  <a href="sdf" className="project-link">
+                      <div className="project">
+                          <div className="project-name">Personal Website</div>
+                          <div className="project-description">
+                              Showcase your portfolio!
+                              <br />
+                              Highlight your skills and achievements.
+                          </div>
+                      </div>
+                  </a>
+              </div>
+          </div>
        </div>
-        
     ); 
-
 }
-export default Projects
+
+export default Projects;
