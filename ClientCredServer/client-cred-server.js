5e3205013f3bf96292cb33c3aae74697dd6bbe70
Sarah Vasquez <savasquez@ucdavis.edu>
2024-07-16 15:56:07 -0700

    update client to lambda

--- ClientCredServer/client-cred-server.js
+++ ClientCredServer/client-cred-server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const SpotifyWebApi = require('spotify-web-api-node');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config({ path: '../.env'}); 
+require('dotenv').config(); 
 
 const app = express();
 app.use(cors());
@@ -28,35 +28,32 @@ const refreshAccessToken = async () => {
   }
 };
 
+
 spotifyApi.clientCredentialsGrant().then(
-    function(data) {
-      console.log('Access Token:', data.body['access_token']);
-      console.log('Expires in:', data.body['expires_in']);
-  
-      spotifyApi.setAccessToken(data.body['access_token']);
-      setInterval(refreshAccessToken, (data.body['expires_in'] - 300) * 1000);
-    },
-    function(err) {
-      console.log('Something went wrong', err);
-    }
-  );
+  function(data) {
+    console.log('Access Token:', data.body['access_token']);
+    console.log('Expires in:', data.body['expires_in']);
+    spotifyApi.setAccessToken(data.body['access_token']);
+    setInterval(refreshAccessToken, (data.body['expires_in'] - 300) * 1000);
+  },
+  function(err) {
+    console.log('Something went wrong', err);
+  }
+);
 
 
-  
-  app.get('/search', async (req, res) => {
-    const { query } = req.query; 
+app.get('/search', async (req, res) => {
+  const { query } = req.query;
 
-    try {
-        const response = await spotifyApi.searchTracks(query);
-        res.json(response.body);
-    } catch (error) {
-        console.error('Error searching tracks:', error);
-        res.status(500).json({ error: 'Failed to search tracks' });
-    }
+  try {
+    const response = await spotifyApi.searchTracks(query);
+    res.json(response.body);
+  } catch (error) {
+    console.error('Error searching tracks:', error);
+    res.status(500).json({ error: 'Failed to search tracks' });
+  }
 });
 
 
-const PORT = 3002;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const serverless = require('serverless-http');
+module.exports.handler = serverless(app);
