753a8da9f88beb3498b300e64d9a5763a945475c
Sarah Vasquez <savasquez@ucdavis.edu>
2024-07-17 10:00:13 -0700

    change redirect uri

--- src/components/SpotifyComponent/SeachAndAdd/Login.js
+++ src/components/SpotifyComponent/SeachAndAdd/Login.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=75b965ea1982472c8166015c2a4c48b9&response_type=code&redirect_uri=http://localhost:3000/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-modify-playback-state%20playlist-modify-public%20playlist-modify-private";
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=75b965ea1982472c8166015c2a4c48b9&response_type=code&redirect_uri=https://main.dwr8v1a0t0gpa.amplifyapp.com/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-modify-playback-state%20playlist-modify-public%20playlist-modify-private";
 
 
 function Login() {
